8498cf1ae2352668a742ea1f99a31dc831dba30c
Francesco Scialdone <francesco.scialdone@beije.it>
2023-03-20 14:31:00 +0100

    Clean Cms.js

--- src/screens/cms/Cms.js
+++ src/screens/cms/Cms.js
@@ -1,7 +1,5 @@
 import Seo from "../../components/functionalComponents/Seo";
 import "./cms.scss";
-import ProductCard from "../../components/functionalComponents/ProductCard/ProductCard";
-import productCardImg from "../../assets/images/productCardImg.jpg";
 
 function Cms() {
   return (
@@ -11,17 +9,6 @@ function Cms() {
         title="Pagina principale"
         description="Descrizione pagina principale"
         content="e-commerce"
-      />
-
-      <ProductCard
-        image={productCardImg}
-        imageAlt="prodotto"
-        id="002103"
-        name="Air Jordan Max Aura"
-        brand="Nike"
-        category="sports shoe"
-        initialPrice="€ 219.99"
-        price="€ 179.99"
       />
     </div>
   );
