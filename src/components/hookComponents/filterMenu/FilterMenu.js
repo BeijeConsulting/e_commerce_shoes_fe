fda3664b862d56ae739a2cf91af7204bf3358dba
Fabrizio Ettori <fabrizio.ettori@beije.it>
2023-03-22 10:20:39 +0100

    fixed functionality

--- src/components/hookComponents/filterMenu/FilterMenu.js
+++ src/components/hookComponents/filterMenu/FilterMenu.js
@@ -7,26 +7,103 @@ import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 function FilterMenu() {
     const minMax = [20, 1000];
+    const sizes = ['m38', 'm39', 'm40', 'm41', 'm42'];
+    const categories = ['sneakers', 'stivali', 'sliders', 'sandali', 'infradito', 'eleganti'];
+    const brands = [
+        "Abercrombie & Fitch",
+        "adidas Golf",
+        "adidas Originals",
+        "adidas performance",
+        "adidas Skateboarding",
+        "ALDO",
+        "AllSaints",
+        "Armani Exchange",
+        "Asics",
+        "ASRA",
+        "Barbour",
+        "Barbour International",
+        "Ben Sherman",
+        "Berghaus",
+        "Bershka",
+        "Birkenstock",
+        "Bolongaro Trevor",
+        "BOSS by Hugo Boss",
+        "Brave Soul",
+        "Buffalo",
+        "Burton Snowboards",
+        "Call it Spring",
+        "Calvin Klein",
+        "Calvin Klein Jeans",
+        "Cat Footwear",
+        "Clarks Originals",
+        "Columbia",
+        "Converse",
+        "Crep Protect",
+        "Crocs",
+        "Devils Advocate",
+        "Dr Martens",
+        "Dune",
+        "EA7",
+        "Farah",
+        "Fred Perry",
+        "French Connection",
+        "Gianni Feraud",
+        "Good For Nothing",
+        "Good News",
+        "Havaianas",
+        "H by Hudson",
+        "Helly Hansen",
+        "Hi-Tec",
+        "Hollister",
+        "HUGO",
+        "Hummel",
+        "Hunter",
+        "Ipanema",
+        "Jack & Jones",
+        "Jason Mark",
+        "Jordan",
+        "Kickers",
+        "Koi Footwear",
+        "Lacoste",
+        "Levi's",
+        "London Rebel X",
+        "Loungeable",
+        "Loyalty & Faith",
+        "Lyle & Scott",
+        "Napapijri",
+        "New Balance",
+        "New Look",
+        "Nike",
+        "Nike Golf",
+        "Nike Running",
+        "Nike SB",
+        "Nike Training",
+        "Noak",
+        "O'Neill",
+        "Oakley",
+        "Office",
+        "On Running",
+        "Original Penguin",
+        "Palladium",
+        "Pier One"];
+    const colors = ['arancione', 'argento', 'bianco', 'blu', 'giallo', 'grigio', 'marrone', 'neutro', 'oro'];
 
     const [state, setState] = useState({
-        active: [],
+        active: null,
         filters: {
             price: [20, 1000],
             onSale: false,
-            genre: '',
-            size: '',
+            genre: null,
+            size: null,
+            category: null,
+            brand: null,
+            color: null,
         }
     });
 
-    console.log(state.filters)
-
     function handleActive(event) {
-        const toActive = event.target.getAttribute('data-filter');
-        let active = [];
-
-        if (!state.active.includes(toActive)) {
-            active.push(toActive);
-        }
+        let active = event.target.getAttribute('data-filter');
+        if (state.active === active) active = null;
 
         setState(
             {
@@ -58,7 +135,7 @@ function FilterMenu() {
 
     function handleGenreChange(e) {
         let choice = e.target.value;
-        if (state.filters.genre === choice) choice = '';
+        if (state.filters.genre === choice) choice = null;
         setState({
             ...state,
             filters: {
@@ -70,7 +147,7 @@ function FilterMenu() {
 
     function handleSizeChange(e) {
         let choice = e.target.value;
-        if (state.filters.size === choice) choice = '';
+        if (state.filters.size === choice) choice = null;
         setState({
             ...state,
             filters: {
@@ -80,14 +157,78 @@ function FilterMenu() {
         });
     };
 
+    function handleCategoryChange(e) {
+        let choice = e.target.value;
+        if (state.filters.category === choice) choice = null;
+        setState({
+            ...state,
+            filters: {
+                ...state.filters,
+                category: choice,
+            }
+        });
+    };
+
+    function handleBrandChange(e) {
+        let choice = e.target.value;
+        if (state.filters.brand === choice) choice = null;
+        setState({
+            ...state,
+            filters: {
+                ...state.filters,
+                brand: choice,
+            }
+        });
+    };
+
+    function handleColorChange(e) {
+        let choice = e.target.value;
+        if (state.filters.color === choice) choice = null;
+        setState({
+            ...state,
+            filters: {
+                ...state.filters,
+                color: choice,
+            }
+        });
+    };
+
+    function mapSizes(item, key) {
+        return <div className="item" key={`${key}-${Math.random()}`}>
+            <input type={'checkbox'} id={`size-${key + 1}`} value={item} onChange={handleSizeChange} checked={state.filters.size === item ? true : false} />
+            <label className="label" htmlFor={`size-${key + 1}`}>{item}</label>
+        </div>
+    }
+
+    function mapCategories(item, key) {
+        return <div className="item" key={`${key}-${Math.random()}`}>
+            <input type={'checkbox'} id={`category-${key + 1}`} value={item} onChange={handleCategoryChange} checked={state.filters.category === item ? true : false} />
+            <label className="label" htmlFor={`category-${key + 1}`}>{item}</label>
+        </div>
+    }
+
+    function mapBrands(item, key) {
+        return <div className="item" key={`${key}-${Math.random()}`}>
+            <input type={'checkbox'} id={`brand-${key + 1}`} value={item} onChange={handleBrandChange} checked={state.filters.brand === item ? true : false} />
+            <label className="label" htmlFor={`brand-${key + 1}`}>{item}</label>
+        </div>
+    }
+
+    function mapColors(item, key) {
+        return <div className="item" key={`${key}-${Math.random()}`}>
+            <input type={'checkbox'} id={`color-${key + 1}`} value={item} onChange={handleColorChange} checked={state.filters.color === item ? true : false} />
+            <label className="label" htmlFor={`color-${key + 1}`}>{item}</label>
+        </div>
+    }
+
     return (
         <aside className="filter-menu">
             <div className="filter-menu__items">
-                <div className={`item ${state.active.includes("price") ? 'active' : ''}`}>
+                <div className={`item ${state.active === "price" ? 'active' : ''}`}>
                     <header onClick={handleActive} data-filter="price" className={`${state.filters.price[0] !== minMax[0] || state.filters.price[1] !== minMax[1] ? 'checked' : ''}`} >
                         <div>prezzo</div>
-                        {!state.active.includes("price") && <KeyboardArrowDownIcon fontSize={'large'} />}
-                        {!!state.active.includes("price") && <KeyboardArrowUpIcon fontSize={'large'} />}
+                        {state.active !== "price" && <KeyboardArrowDownIcon fontSize={'large'} />}
+                        {state.active === "price" && <KeyboardArrowUpIcon fontSize={'large'} />}
                     </header>
                     <div className="sub-item">
                         <div className="item">
@@ -110,25 +251,25 @@ function FilterMenu() {
                     </div>
                 </div>
 
-                <div className={`item ${state.active.includes("on-sale") ? 'active' : ''}`}>
+                <div className={`item ${state.active === "on-sale" ? 'active' : ''}`}>
                     <header onClick={handleActive} data-filter="on-sale" className={`${!!state.filters.onSale ? 'checked' : ''}`} >
                         <div>Ribassi</div>
-                        {!state.active.includes("on-sale") && <KeyboardArrowDownIcon fontSize={'large'} />}
-                        {!!state.active.includes("on-sale") && <KeyboardArrowUpIcon fontSize={'large'} />}
+                        {state.active !== "on-sale" && <KeyboardArrowDownIcon fontSize={'large'} />}
+                        {state.active === "on-sale" && <KeyboardArrowUpIcon fontSize={'large'} />}
                     </header>
                     <div className="sub-item">
                         <div className="item">
-                            <input onClick={handleSaleChange} type={'checkbox'} id="on-sale1" />
-                            <label className="label" htmlFor="on-sale1">scarpe in ribasso</label>
+                            <input onClick={handleSaleChange} type={'checkbox'} id="on-sale" />
+                            <label className="label" htmlFor="on-sale">scarpe in ribasso</label>
                         </div>
                     </div>
                 </div>
 
-                <div className={`item ${state.active.includes("genre") ? 'active' : ''}`}>
+                <div className={`item ${state.active === "genre" ? 'active' : ''}`}>
                     <header onClick={handleActive} data-filter="genre" className={`${state.filters.genre ? 'checked' : ''}`} >
                         <div>Genere</div>
-                        {!state.active.includes("genre") && <KeyboardArrowDownIcon fontSize={'large'} />}
-                        {!!state.active.includes("genre") && <KeyboardArrowUpIcon fontSize={'large'} />}
+                        {state.active !== "genre" && <KeyboardArrowDownIcon fontSize={'large'} />}
+                        {state.active === "genre" && <KeyboardArrowUpIcon fontSize={'large'} />}
                     </header>
                     <div className="sub-item">
                         <div className="item">
@@ -146,28 +287,50 @@ function FilterMenu() {
                     </div>
                 </div>
 
-                <div className={`item ${state.active.includes("size") ? 'active' : ''}`}>
-                    <header onClick={handleActive} data-filter="size" className={`${state.filters.size ? 'checked' : ''}`} >
+                <div className={`item ${state.active === "size" ? 'active' : ''
+                    } `}>
+                    <header onClick={handleActive} data-filter="size" className={`${state.filters.size ? 'checked' : ''} `} >
                         <div>taglia</div>
-                        {!state.active.includes("size") && <KeyboardArrowDownIcon fontSize={'large'} />}
-                        {!!state.active.includes("size") && <KeyboardArrowUpIcon fontSize={'large'} />}
+                        {state.active !== "size" && <KeyboardArrowDownIcon fontSize={'large'} />}
+                        {state.active === "size" && <KeyboardArrowUpIcon fontSize={'large'} />}
                     </header>
                     <div className="sub-item">
-                        <div className="item">
-                            <input type={'checkbox'} id="size1" value={'eu 38'} onChange={handleSizeChange} checked={state.filters.size === 'eu 38' ? true : false} />
-                            <label className="label" htmlFor="size1">eu 38</label>
-                        </div>
-                        <div className="item">
-                            <input type={'checkbox'} id="size2" value={'eu 39'} onChange={handleSizeChange} checked={state.filters.size === 'eu 39' ? true : false} />
-                            <label className="label" htmlFor="size2">eu 39</label>
-                        </div>
-                        <div className="item">
-                            <input type={'checkbox'} id="size3" value={'eu 40'} onChange={handleSizeChange} checked={state.filters.size === 'eu 40' ? true : false} />
-                            <label className="label" htmlFor="size3">eu 40</label>
-                        </div>
+                        {state.active === 'size' && sizes.map(mapSizes)}
                     </div>
                 </div>
 
+                <div className={`item ${state.active === "category" ? 'active' : ''} `}>
+                    <header onClick={handleActive} data-filter="category" className={`${state.filters.category ? 'checked' : ''} `} >
+                        <div>categoria</div>
+                        {state.active !== "category" && <KeyboardArrowDownIcon fontSize={'large'} />}
+                        {state.active === "category" && <KeyboardArrowUpIcon fontSize={'large'} />}
+                    </header>
+                    <div className="sub-item">
+                        {state.active === "category" && categories.map(mapCategories)}
+                    </div>
+                </div>
+
+                <div className={`item ${state.active === "brand" ? 'active' : ''} `}>
+                    <header onClick={handleActive} data-filter="brand" className={`${state.filters.brand ? 'checked' : ''} `} >
+                        <div>brand</div>
+                        {state.active !== "brand" && <KeyboardArrowDownIcon fontSize={'large'} />}
+                        {state.active === "brand" && <KeyboardArrowUpIcon fontSize={'large'} />}
+                    </header>
+                    <div className="sub-item">
+                        {state.active === "brand" && brands.map(mapBrands)}
+                    </div>
+                </div>
+
+                <div className={`item ${state.active === "color" ? 'active' : ''} `}>
+                    <header onClick={handleActive} data-filter="color" className={`${state.filters.color ? 'checked' : ''} `} >
+                        <div>colore</div>
+                        {state.active !== "color" && <KeyboardArrowDownIcon fontSize={'large'} />}
+                        {state.active === "color" && <KeyboardArrowUpIcon fontSize={'large'} />}
+                    </header>
+                    <div className="sub-item">
+                        {state.active === "color" && colors.map(mapColors)}
+                    </div>
+                </div>
             </div>
         </aside>
     )
