070941fa75cdf9e5db27bd272c7a7aceedc2f1da
Luigi Lesca <luigi.lesca@beije.it>
2023-03-22 22:14:58 +0100

    create contacts screen

---
+++ src/screens/contacts/Contacts.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import "./contacts.scss";
+import { Link, NavLink } from 'react-router-dom';
+
+function Contacts() {
+    return (
+        <div className='contacts'>
+            <h1>Contatti</h1>
+            <h3>
+                Se hai difficoltà o dubbi relativi allo shop online, il nostro Servizio Clienti è a tua disposizione. Scopri come contattarci cliccando <span>
+                    <Link to={ "/" }>
+                        quì.
+                    </Link>
+                </span>
+            </h3>
+
+            <Link to={ "/customerCare" }>
+                Torna all'inizio
+            </Link>
+        </div>
+    )
+}
+
+Contacts.defaultProps = {
+
+}
+
+Contacts.propTypes = {
+
+}
+
+export default Contacts;
