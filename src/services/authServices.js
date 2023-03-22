a04e5163ada5511672894740e1b83dbf159f2e45
Francesco Scialdone <francesco.scialdone@beije.it>
2023-03-22 12:40:37 +0100

    Setup API services: genericAxios, addressServices, authServices, orderServices, productServices

---
+++ src/services/authServices.js
@@ -0,0 +1,25 @@
+import { getData, postData } from "../genericAxios/genericAxios";
+
+export async function authCheck() {
+  const response = await getData("/check");
+
+  return { status: response.status, data: response.data };
+}
+
+export async function signin(credentials) {
+  const response = await postData("/signin", credentials);
+
+  return { status: response.status, data: response.data };
+}
+
+export async function signout(refreshTokenObj, SECRET) {
+  const response = await postData("/sign_out", refreshTokenObj, SECRET);
+
+  return { status: response.status, data: response.data };
+}
+
+export async function refreshToken(refreshTokenObj, SECRET) {
+  const response = await postData("/refresh_token", refreshTokenObj, SECRET);
+
+  return { status: response.status, data: response.data };
+}
