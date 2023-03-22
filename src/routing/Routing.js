7897444740da2954bc6567eb3a85976775a9f257
Luigi Lesca <luigi.lesca@beije.it>
2023-03-22 22:16:37 +0100

    edit routing

--- src/routing/Routing.js
+++ src/routing/Routing.js
@@ -4,15 +4,28 @@ import Cms from "../screens/cms/Cms";
 import Identity from "../screens/identity/Identity";
 import SignupForm from "../components/hookComponents/signupForm/SignupForm";
 import LoginForm from "../components/hookComponents/loginForm/LoginForm";
+import CustomerCare from '../screens/customerCare/CustomerCare';
+import Contacts from '../screens/contacts/Contacts';
+import Delivery from '../screens/delivery/Delivery';
+import ReturnAndRefund from '../screens/returnsAndRefund/ReturnAndRefund';
 
 function Routing() {
   return (
     <Routes>
-      <Route path="/" element={<Cms />} />
-      <Route path="/identity" element={<Identity />}>
-        <Route index element={<LoginForm />} />
-        <Route path="signup" element={<SignupForm />} />
+      <Route path="/" element={ <Cms /> } />
+      <Route path="/identity" element={ <Identity /> }>
+        <Route index element={ <LoginForm /> } />
+        <Route path="signup" element={ <SignupForm /> } />
       </Route>
+
+      <Route path='/customerCare' element={ <CustomerCare /> }>
+        <Route path='contacts' element={ <Contacts /> } />
+        <Route path='delivery' element={ <Delivery /> } />
+        <Route path='returnAndRefund' element={ <ReturnAndRefund /> } />
+      </Route>
+
+
+
     </Routes>
   );
 }
