600301e1a278933d3135926f7f54217698cb0c94
Luigi Lesca <luigi.lesca@beije.it>
2023-03-22 22:16:20 +0100

    create returnsAndRefund screen

---
+++ src/screens/returnsAndRefund/ReturnAndRefund.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import "./returnAndrefund.scss"
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+
+function ReturnAndRefund() {
+    return (
+        <div className='returns'>
+            <h1>Resi e Rimborsi</h1>
+            <p>
+                Se hai effettuato un reso il rimborso avverrà entro 14 giorni dalla data di consegna del pacco al corriere. Non appena il reso sarà ricevuto e controllato ti invieremo un'email di conferma. Nel caso tu abbia pagato le spese per la spedizione standard e decida di rendere l'ordine per intero, queste ti verranno rimborsate.
+            </p>
+
+            <h3>Hai pagato in contanti alla consegna?</h3>
+            <p>
+                Durante la procedura di reso online potrai scegliere il metodo di rimborso che preferisci, tra nota di credito (credito Zalando) o bonifico bancario.</p>
+            <p>
+                Nel caso in cui scegliessi il bonifico, sarà necessario inserire le tue coordinate bancarie (nome completo dell’intestatario del conto, IBAN e BIC).
+            </p>
+
+            <h3>Hai pagato con Paypal?</h3>
+            <p>Riceverai la cifra relativa agli articoli resi sullo stesso account Paypal.</p>
+
+            <Link to={ "/customerCare" }>
+                Torna all'inizio
+            </Link>
+        </div>
+    )
+}
+
+ReturnAndRefund.defaultProps = {
+
+}
+
+ReturnAndRefund.propTypes = {
+
+}
+
+export default ReturnAndRefund;
