d5427cb5eae23472942df29489ee05da77a6f6d0
Luigi Lesca <luigi.lesca@beije.it>
2023-03-22 22:15:43 +0100

    create delivery screen

---
+++ src/screens/delivery/Delivery.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import "./delivery.scss"
+import { Link } from 'react-router-dom';
+
+
+function Delivery() {
+    return (
+        <div className='delivery'>
+            <h1>Spedizioni</h1>
+            <h3>Tempi e costi di spedizione</h3>
+            <p>
+                La spedizione è gratuita per la maggior parte degli articoli per ordini superiori a 28,90 €.
+            </p>
+
+            <p>
+                Per quelli il cui importo è inferiore a 28,90 €, il costo di spedizione è di 4,95 €.
+            </p>
+
+            <p>
+                Gli articoli da lunga distanza non rientrano questa offerta. Gli articoli devono essere spediti da un magazzino più lontano, perciò è previsto un costo di consegna da lunga distanza di 4,95 €.
+            </p>
+
+            <p>
+                Se il valore dell'ordine è inferiore al valore minimo di 28,90 € e comprende anche un articolo per la consegna a distanza, si dovrà pagare solo la tariffa di consegna standard, non entrambe.
+            </p>
+
+            <p>
+                In caso di reso o di annullamento dell'ordine, la tariffa di consegna standard e la tariffa di consegna da lunga distanza vengono rimborsate solo in caso di restituzione dell'intero ordine, non in caso di restituzione parziale.
+            </p>
+            <Link to={ "/customerCare" }>
+                Torna all'inizio
+            </Link>
+        </div>
+    )
+}
+
+Delivery.defaultProps = {
+
+}
+
+Delivery.propTypes = {
+
+}
+
+export default Delivery;
