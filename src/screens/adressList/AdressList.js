7808a822cf0543de86ec027c6d7b885017c5183d
Luigi Lesca <luigi.lesca@beije.it>
2023-03-27 16:15:47 +0200

    fix adressList

--- src/screens/adressList/AdressList.js
+++ src/screens/adressList/AdressList.js
@@ -1,15 +1,79 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 
+// Redux
+import { useSelector } from "react-redux";
+// Router
+import { useNavigate } from 'react-router-dom';
+// Utils
+import { getLocalStorage } from '../../utils/localStorageUtils';
+// SCSS
 import "./adressList.scss";
 
 function AdressList(props) {
   const userData = useSelector((state) => state.userDuck);
+  const navigate = useNavigate()
+
+  // if user is not logged --> go to identityScreen 
+  useEffect(() => {
+    const localStorage = getLocalStorage("token")
+    if (!localStorage) navigate("/identity")
+  }, [])
+
+
+  function mapList(data, i) {
+    return (
+      <div className='address__container' key={ data.id }>
+        <h3>Indirizzo n. { i + 1 }</h3>
+        <ul>
+          <li>
+            <p className='address__p'>Paese:</p>
+            <span>
+              { data?.country }
+            </span>
+          </li>
+          <li>
+            <p className='address__p'>Cap:</p>
+            <span>
+              { data?.zipcode }
+            </span>
+          </li>
+          <li>
+            <p className='address__p'>Nome e cognome:</p>
+            <span>
+              { data?.name_surname }
+            </span>
+          </li>
+          <li>
+            <p className='address__p'>Indirizzo:</p>
+            <span>
+              { data?.street_address }
+            </span>
+          </li>
+          <li>
+            <p className='address__p'>Numero di telefono:</p>
+            <span>
+              { data?.telephone }
+            </span>
+          </li>
+          <li>
+            <p className='address__p'>Istruzioni per il corriere:</p>
+            <span>
+              { data?.instructions }
+            </span>
+          </li>
+        </ul>
+      </div>
+    )
+  }
 
   return (
-    <div>
-      <h2>I tui Indirizzi</h2>
+    <div className='address'>
+      <h2>I tuoi Indirizzi</h2>
+      { userData.adresses?.map(mapList) }
+      { userData.adresses?.length === 0 && <div>
+        <p>Non hai inserito nessun indirizzo.</p>
+      </div> }
     </div>
   );
 }
