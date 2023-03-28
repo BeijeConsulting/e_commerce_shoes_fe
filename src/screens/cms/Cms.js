8b772a5a10aed7ff18e6175655a2e4c38814a7cd
Luigi Lesca <luigi.lesca@beije.it>
2023-03-28 16:19:47 +0200

    edit cms

--- src/screens/cms/Cms.js
+++ src/screens/cms/Cms.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./cms.scss";
 import Seo from "../../components/functionalComponents/Seo";
 import Header from "../../components/hookComponents/header/Header";
 import PreFooter from "../../components/hookComponents/preFooter/PreFooter";
 import Footer from "../../components/functionalComponents/footer/Footer";
 
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import i18n from '../../assets/translations/i18n';
 
 function Cms() {
+  const navigate = useNavigate()
+  const { pathname } = useLocation()
+  // console.log("PATHNAME CMS", pathname)
+
+
+  useEffect(() => {
+    const langs = ["it", "en"];
+    const isPathLang = langs.some((l) => pathname.includes(l))
+
+    if (isPathLang) {
+      return
+    } else {
+      navigate(`/${i18n.language.slice(0, 2)}${pathname}`)
+    }
+
+  }, [pathname])
+
   return (
     <div className="cms">
       <Seo
