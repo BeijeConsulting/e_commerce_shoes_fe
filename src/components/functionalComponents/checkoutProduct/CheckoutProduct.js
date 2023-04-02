148641fba8ff44165a4f4c7a78c32f055b9f6832
Luigi Lesca <luigi.lesca@beije.it>
2023-04-02 17:20:44 +0200

    add translations

--- src/components/functionalComponents/checkoutProduct/CheckoutProduct.js
+++ src/components/functionalComponents/checkoutProduct/CheckoutProduct.js
@@ -1,20 +1,21 @@
 import React from "react";
+// SCSS
 import "./checkoutProduct.scss";
 
 function CheckoutProduct(props) {
     return (
         <article className="checkout-product">
             <div className="__image">
-                <img src={props.productSrc} alt={props.productAlt} />
+                <img src={ props.productSrc } alt={ props.productAlt } />
             </div>
             <div className="__info">
-                <div className="__price">{props.productPrice}€</div>
-                <div className="__name">{props.productName}</div>
+                <div className="__price">{ props.productPrice }€</div>
+                <div className="__name">{ props.productName }</div>
                 <div className="__color-size">
-                    <div>{props.productColor}</div>
-                    <div>{props.productSize}</div>
+                    <div>{ props.productColor }</div>
+                    <div>{ props.productSize }</div>
                 </div>
-                <div>Qtà: <span className="__quantity">{props.productQuantity}</span></div>
+                <div>Qtà: <span className="__quantity">{ props.productQuantity }</span></div>
             </div>
         </article>
     )
